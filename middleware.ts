ea5fe677a6a98f8053fa0850437081c93d9b2014
Maximilian Cassis Krizmanic <Maximilian-Cassis@web.de>
2026-03-25 15:39:49 +0100

    fix: standard clerk middleware and update

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware()
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Ignoriert interne Next.js Dateien und statische Assets (Bilder, etc.)
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Stellt sicher, dass die Middleware für API-Routen ausgeführt wird
     '/(api|trpc)(.*)',
   ],
-}
+};
